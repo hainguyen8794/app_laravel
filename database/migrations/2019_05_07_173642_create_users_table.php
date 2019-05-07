9c9808243f42bed12735153aafdc57f24aa7b5d5
hainh8312 <hainh8312@co-well.com.vn>
2019-05-07 17:42:20 +0700

    migration

---
+++ database/migrations/2019_05_07_173642_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('password');
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->integer('village_id')->unsigned();
+            $table->foreign('village_id')->references('id')->on('village');
+            $table->integer('point')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
